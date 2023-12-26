ee4ed34065064760b2f92eba80166153535aa138
manav083 <manav@locad.net>
2023-12-26 21:35:41 +0530

    episode 5 completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,41 +1,85 @@
-import RestaurantCard from "./RestaurantCard"
-import { useState, useEffect } from "react";
-// import { resData } from "../utils/mockData";
+import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import { resData } from "../utils/mockData";
 
 
 const Body = () => {
 
-    const [listofRestaurant, setListOfRestaurant] = useState([]);
+  // State Variable - Super Powerful variable
+  const [listOfRestaurants, setListOfRestaurants] = useState(resData);
 
-    useEffect(() => {
-        // this callback function will be called after the component is rendered.
-        // console.log("useEffect Called");
-        fetchData();
-    }, []);
+  // Normal JS variable
+  // let listOfRestaurants = [
+  //   {
+  //     "info": {
+  //       "id": "368133",
+  //       "name": "White Hart Pizza",
+  //       "cloudinaryImageId": "zxxuxplfux7eoxsv7ms2",
+  //       "costForTwo": "₹280 for two",
+  //       "cuisines": [
+  //         "Pizzas",
+  //         "Italian",
+  //         "Snacks"
+  //       ],
+  //       "avgRating": 3.8,
+  //       "sla": {
+  //         "deliveryTime": 33,
+  //       },
+  //     }
+  //   },
+  //   {
+  //     "info": {
+  //       "id": "368134",
+  //       "name": "Dominos",
+  //       "cloudinaryImageId": "zxxuxplfux7eoxsv7ms2",
+  //       "costForTwo": "₹280 for two",
+  //       "cuisines": [
+  //         "Pizzas",
+  //         "Italian",
+  //         "Snacks"
+  //       ],
+  //       "avgRating": 4.4,
+  //       "sla": {
+  //         "deliveryTime": 33,
+  //       },
+  //     }
+  //   },
+  //   {
+  //     "info": {
+  //       "id": "368135",
+  //       "name": "Mc Donald's",
+  //       "cloudinaryImageId": "zxxuxplfux7eoxsv7ms2",
+  //       "costForTwo": "₹280 for two",
+  //       "cuisines": [
+  //         "Pizzas",
+  //         "Italian",
+  //         "Snacks"
+  //       ],
+  //       "avgRating": 4.1,
+  //       "sla": {
+  //         "deliveryTime": 33,
+  //       },
+  //     }
+  //   }
+  // ];
 
-    const fetchData = async () =>  {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-        const json = await data.json();
 
-        // setListOfRestaurant(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants); // Not a good way or standard way
-
-        // Always use Optional Chaining
-        setListOfRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants); // Not a good way or standard way
-        // console.log(json);
-    }
-
-    // console.log("Body rendered"); // this will be called before useEffect
-
-    return (
-        <div className="body">
-            <div className="Search">
-                <button onClick={() => { setListOfRestaurant(listofRestaurant.filter((r) => r.info.avgRating >= 4)) }}>Top Rated Restaurant</button>
-            </div>
-            <div className="restaurant-container">
-                {listofRestaurant.map((r) => <RestaurantCard key={r.info.id} resData={r} />)}
-            </div>
-        </div>
-    )
+  return (
+    <div className="body">
+      <div className="filter">
+        <button className="filter-btn"
+          onClick={() => {
+            setListOfRestaurants(listOfRestaurants.filter((r) => r.info.avgRating >= 4.0));
+          }}>
+          Top Rated Restauants
+        </button>
+        {/* Filter restaurants whose rating is greater than 4.0 */}
+      </div>
+      <div className="restaurant-container">
+        {listOfRestaurants.map((r) => <RestaurantCard key={r.info.id} resData={r} />)}
+      </div>
+    </div>
+  )
 }
 
 // default export 
